c04f3be4c94b3d4a6da447b665eede732c9155ed
lbwa <mr_lbw@outlook.com>
2020-02-06 00:43:29 +0800

    feat: complete observer pattern

--- packages/observer/__tests__/observer.spec.ts
+++ packages/observer/__tests__/observer.spec.ts
@@ -1,8 +1,47 @@
-import { Observer } from '../src'
+import { Subject } from '../src'
 
-describe('observer pattern', () => {
-  it('create a observer', () => {
-    const observer = new Observer()
-    expect(observer).toBeDefined()
+describe('Subject', () => {
+  it('create a subject', () => {
+    const subject = new Subject()
+    expect(subject).toBeDefined()
+    expect(subject.register).toBeDefined()
+    expect(subject.unregister).toBeDefined()
+    expect(subject.notify).toBeDefined()
+  })
+
+  it('register a observer', () => {
+    const subject = new Subject()
+    const mockObserver = () => 1 + 1
+    subject.register(mockObserver)
+    expect(subject.has(mockObserver)).toBeTruthy()
+  })
+
+  it('unregister a observer', () => {
+    const subject = new Subject()
+    const mockObserver = () => 1 + 1
+    subject.register(mockObserver)
+    subject.unregister(mockObserver)
+    expect(subject.has(mockObserver)).toBeFalsy()
+  })
+
+  it('notify all observer once or multiple times', () => {
+    const subject = new Subject()
+    const mockObserver = jest.fn((message: any) => message)
+    const mockOtherObserver = jest.fn((message: any) => message)
+    const firstMessage = 'first message'
+    const secondMessage = 'second message'
+    subject.register(mockObserver)
+    subject.register(mockOtherObserver)
+    subject.notify(firstMessage)
+    expect(mockObserver).toBeCalledTimes(1)
+    expect(mockOtherObserver).toBeCalledTimes(1)
+    expect(mockObserver.mock.calls[0][0]).toBe(firstMessage)
+    expect(mockOtherObserver.mock.calls[0][0]).toBe(firstMessage)
+
+    subject.notify(secondMessage)
+    expect(mockObserver).toBeCalledTimes(2)
+    expect(mockOtherObserver).toBeCalledTimes(2)
+    expect(mockObserver.mock.calls[1][0]).toBe(secondMessage)
+    expect(mockOtherObserver.mock.calls[1][0]).toBe(secondMessage)
   })
 })
--- packages/observer/src/index.ts
+++ packages/observer/src/index.ts
@@ -1,5 +1,39 @@
-export class Observer {
-  constructor() {}
+class Observers {
+  private list = new Set<Function>()
 
-  notify() {}
+  register(observer: Function) {
+    return this.list.add(observer)
+  }
+
+  unregister(observer: Function) {
+    return this.list.delete(observer)
+  }
+
+  has(observer: Function) {
+    return this.list.has(observer)
+  }
+
+  update<T = any>(message: T) {
+    this.list.forEach(observer => observer(message))
+  }
+}
+
+export class Subject {
+  private observers = new Observers()
+
+  register(observer: Function) {
+    return this.observers.register(observer)
+  }
+
+  unregister(observer: Function) {
+    return this.observers.unregister(observer)
+  }
+
+  has(observer: Function) {
+    return this.observers.has(observer)
+  }
+
+  notify(message: any) {
+    this.observers.update(message)
+  }
 }
